da875fda5af710ab2a7c0d58c3348d48f14f0c63
kimhyungho <68440536+kimhyungho@users.noreply.github.com>
2020-12-18 11:58:36 +0900

    Update info.js

--- front/pages/info.js
+++ front/pages/info.js
@@ -18,4 +18,15 @@ const Info = () => {
     );
 
 };
+        
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+    console.log('getServerSideProps start');
+    console.log(context.req.headers);
+    const cookie = context.req ? context.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
+    if(context.req && cookie) {
+        axios.defaults.headers.Cookie = cookie;
+    }
+});z
+        
 export default Info;
