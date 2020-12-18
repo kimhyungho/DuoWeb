94e32dffaa94ac28ca0bf13fccdaacf778665fec
kimhyungho <68440536+kimhyungho@users.noreply.github.com>
2020-12-18 11:59:17 +0900

    Update event.js

--- front/pages/event.js
+++ front/pages/event.js
@@ -11,5 +11,13 @@ const Event = () => {
     );
 
 };
-
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+    console.log('getServerSideProps start');
+    console.log(context.req.headers);
+    const cookie = context.req ? context.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
+    if(context.req && cookie) {
+        axios.defaults.headers.Cookie = cookie;
+    }
+});
 export default Event;
